c917e645ae4082c30c41324b92bbf484750e25cb
Stelios Zisis <Support@ntclabs.net>
2026-02-24 14:53:20 +0200

    feat: use collection expressions for list initialization

--- Services/HistoricalMetricsExportService.cs
+++ Services/HistoricalMetricsExportService.cs
@@ -284,8 +284,8 @@ public class HistoricalMetricsExportService : IHistoricalMetricsExportService
 
 public class HistoricalExportData
 {
-    public List<PerformanceMetrics> PerformanceMetrics { get; set; } = new();
-    public List<UmbracoMetrics> UmbracoMetrics { get; set; } = new();
+    public List<PerformanceMetrics> PerformanceMetrics { get; set; } = [];
+    public List<UmbracoMetrics> UmbracoMetrics { get; set; } = [];
     public ExportOptions ExportOptions { get; set; } = new();
     public DateTime GeneratedAt { get; set; }
     public int RecordCount { get; set; }
