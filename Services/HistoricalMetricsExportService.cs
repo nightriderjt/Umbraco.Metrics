dab9f636ab45417ac238a881b5c21d9dde709dd1
Stelios Zisis <Support@ntclabs.net>
2026-02-24 14:54:39 +0200

    feat: remove HistoricalExportData class from export service file and relocated to Models

--- Services/HistoricalMetricsExportService.cs
+++ Services/HistoricalMetricsExportService.cs
@@ -281,12 +281,3 @@ public class HistoricalMetricsExportService : IHistoricalMetricsExportService
         return Encoding.UTF8.GetBytes(xmlBuilder.ToString());
     }
 }
-
-public class HistoricalExportData
-{
-    public List<PerformanceMetrics> PerformanceMetrics { get; set; } = [];
-    public List<UmbracoMetrics> UmbracoMetrics { get; set; } = [];
-    public ExportOptions ExportOptions { get; set; } = new();
-    public DateTime GeneratedAt { get; set; }
-    public int RecordCount { get; set; }
-}
